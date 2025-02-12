71d64f867020c73d06114a7b9b335bb007aaca30
SofiePedersen <sofie@gefiber.dk>
2025-02-12 12:34:30 +0100

    testing

--- burger.js
+++ burger.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     $("#galleri").load("slide.html");
 });
+
+document.addEventListener("DomContentLoaded", function() {
+    $("#login__mikkel").load("login.html");
+});
