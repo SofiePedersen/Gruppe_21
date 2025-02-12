fe806083397e9fc290353e5f6ed1c95fc3aefac1
Peternl98 <pnla67088@edu.ucl.dk>
2025-02-12 14:08:58 +0100

    kontakt v3
    forsøg på mediaquery

--- kontakt.js
+++ kontakt.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let popupVisible = false;
     const toggleButton = document.getElementById("togglePopup");
     const popupMenu = document.getElementById("popupMenu");
-    const form = document.getElementById("contactForm");
+    const form = document.getElementById("kontaktForm");
     const emailInput = document.getElementById("email");
     const checkboxes = document.querySelectorAll(".valgmulighed");
 
@@ -26,13 +26,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
         let email = emailInput.value;
         if (!email.includes("@")) {
-            alert("Indtast venligst en gyldig e-mail.");
+            alert("Indtast venligst en gyldig e-mail");
             return;
         }
 
 
         let selectedMode = document.querySelector('input[name="mode"]:checked');
-        let modeValue = selectedMode ? selectedMode.value : "Ingen valg"; // Hvis ingen er valgt, sæt det til "Ingen valg"
+        let modeValue = selectedMode ? selectedMode.value : "Ingen valg"; // Hvis ingen er valgt, vises "Ingen valg" i consol.log return
 
     
         let valgmulighed = [];
